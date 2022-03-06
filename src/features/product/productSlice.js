17af90cdd9eb4fe3e6a241250c216106ae84abf9
Xabi Martínez <xabi.ma@gmail.com>
2022-03-06 14:17:04 +0100

    Add getProduct reducer to productSlice

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -7,12 +7,24 @@ const fetchProducts = async () => {
     }
 };
 
+const fetchProduct = async id => {
+    const res = await fetch(`https://dulces-petalos.herokuapp.com/api/product/${id}`);
+    if (res.ok) {
+        return res.json();
+    }
+};
+
 export const getProductsAsync = createAsyncThunk("products/fetchProducts", async () => {
     return fetchProducts();
 });
 
+export const getProductAsync = createAsyncThunk("products/fetchProduct", async id => {
+    return fetchProduct(id);
+});
+
 const initialState = {
-    products: []
+    products: [],
+    product: null
 };
 
 const reducers = {};
@@ -21,6 +33,9 @@ const extraReducers = builder => {
     builder
         .addCase(getProductsAsync.fulfilled, (state, action) => {
             state.products = action.payload;
+        })
+        .addCase(getProductAsync.fulfilled, (state, action) => {
+            state.product = action.payload;
         });
 };
 
@@ -32,5 +47,6 @@ export const productSlice = createSlice({
 });
 
 export const selectProducts = state => state.product.products;
+export const selectProduct = state => state.product.product;
 
 export default productSlice.reducer;
