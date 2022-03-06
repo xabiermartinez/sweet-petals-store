3ce1eb4c94d7ea9224223b6b8d2f98773fcb142a
Xabi Martínez <xabi.ma@gmail.com>
2022-03-06 10:44:31 +0100

    Add Router with Products route

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,16 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 test("renders learn react link", () => {
     const { getByText } = render(
         <Provider store={store}>
-            <App />
+            <Router>
+                <App />
+            </Router>
         </Provider>
     );
 
-    expect(getByText(/learn/i)).toBeInTheDocument();
+    expect(getByText(/Sweet Petals Store/i)).toBeInTheDocument();
 });
