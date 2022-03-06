224a53678328248306ce0f4eca4fc9b05894f6b2
Xabi Martínez <xabi.ma@gmail.com>
2022-03-06 14:17:16 +0100

    Add Product screen

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Products from "./screens/Products";
 import Header from "./components/Header";
+import Product from "./screens/Product";
 
 function App () {
     return (
         <div>
             <Header />
             <Routes>
+                <Route path="/products/:productId" element={<Product />} />
                 <Route path="/" element={<Products />} />
             </Routes>
         </div>
